a33091daa3eb7069b2b40f47ba32021e1a94101c
Pedro <priccis@users.noreply.github.com>
2024-07-29 20:39:03 -0400

    feat: SSR no hydration

---
+++ 2-server-side-rendering-no-hydration/index.js
@@ -0,0 +1,21 @@
+import { renderToString } from 'react-dom/server';
+
+import App from './App';
+
+Bun.serve({
+  async fetch() {
+    const appString = renderToString(<App />);
+    const html = `
+      <!DOCTYPE html>
+      <html>
+        <body>
+          <div id="root">${appString}</div>
+        </body>
+      </html>
+    `
+    return new Response(html, {
+      headers: { "Content-Type": "text/html" },
+    });
+  },
+  port: 3444,
+});
