d6ba85cb922b8af7216bf55f10f2e7aa0fb79afa
Pedro <30800044+pricci1@users.noreply.github.com>
2024-08-30 10:02:17 -0400

    refactor: multiline to clarify

--- 1-client-side-rendering/client.js
+++ 1-client-side-rendering/client.js
@@ -2,7 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const rootDiv = document.getElementById('root')
+const reactRoot = ReactDOM.createRoot(rootDiv)
+
+reactRoot.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
